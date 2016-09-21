b4df4954bf38b0131771da9a2a6ba666fa5460f3
akbeeram <anilkumarb404@gmail.com>
2016-09-21 17:04:56 +0530

    Update LandingController.js

--- js/controllers/LandingController.js
+++ js/controllers/LandingController.js
@@ -1,1 +1,9 @@
-
+(angular)(){
+  'use strict';
+  
+  var landing = angular.module('LandingModule',[]);
+  
+  landing.controller('LandingController',function($scope){
+    $scope.msg = "Hello";
+  });
+}
