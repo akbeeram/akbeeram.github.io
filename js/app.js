252025f821a8534bf3598720fc4cb88080f00bd0
akbeeram <anilkumarb404@gmail.com>
2016-09-21 19:41:51 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,28 +1,28 @@
 (function(angular){
   'use strict';
   
-  var app = angular.module('akbeeram',['ngRoute','LandingModule']);
+  var app = angular.module('akbeeram',['ngRoute','HomeModule']);
   app.config(function($routeProvider){
     $routeProvider
       .when("/",{
         templateUrl:'../partials/home.html',
-        controller:'LandingController'
+        controller:'HomeController'
       })
       .when("/work",{
         templateUrl:'../partials/work.html',
-        controller:'LandingController'
+        controller:'HomeController'
       })
       .when("/crafts",{
         templateUrl:'../partials/crafts.html',
-        controller:'LandingController'
+        controller:'HomeController'
       })
       .when("/about",{
         templateUrl:'../partials/about.html',
-        controller:'LandingController'
+        controller:'HomeController'
       })
       .when("/contact",{
         templateUrl:'../partials/contact.html',
-        controller:'LandingController'
+        controller:'HomeController'
       })
   });
 }(angular));
