c845ed56cf5d31163771e895d9967d9a42aa2923
akbeeram <anilkumarb404@gmail.com>
2016-09-21 17:02:17 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-
+(angular)(){
+  'use strict';
+  
+  var app = angular.module('akbeeram',['ngRoute','LandingModule']);
+  app.config(function($routeProvider){
+    $routeProvider
+      .when("/",{
+        templateUrl:'partials/home.html'
+      })
+  });
+}
