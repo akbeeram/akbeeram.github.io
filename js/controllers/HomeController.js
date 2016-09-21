d0d01d9929ea001fd98b7ad35d6413c66467fd24
akbeeram <anilkumarb404@gmail.com>
2016-09-21 19:51:15 +0530

    Update HomeController.js

--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -4,18 +4,18 @@
   var landing = angular.module('HomeModule',[]);
   
   landing.controller('HomeController',function($scope){
-    $scope.msg = "Hello";
+    $scope.msg = "Hello1";
   });
   landing.controller('WorkController',function($scope){
-    $scope.msg = "Hello";
+    $scope.msg = "Hello2";
   });
   landing.controller('CraftsController',function($scope){
-    $scope.msg = "Hello";
+    $scope.msg = "Hello3";
   });
   landing.controller('AboutController',function($scope){
-    $scope.msg = "Hello";
+    $scope.msg = "Hello4";
   });
   landing.controller('ContactController',function($scope){
-    $scope.msg = "Hello";
+    $scope.msg = "Hello5";
   });
 }(angular));
