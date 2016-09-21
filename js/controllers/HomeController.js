b2ba058c0cf9bae6dbec2836eb2577124bb117a9
akbeeram <anilkumarb404@gmail.com>
2016-09-21 19:53:36 +0530

    Update HomeController.js

--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -6,16 +6,16 @@
   landing.controller('HomeController',function($scope, $rootScope){
     $rootScope.msg = "Hello1";
   });
-  landing.controller('WorkController',function($scope){
-    $scope.msg = "Hello2";
+  landing.controller('WorkController',function($scope, $rootScope){
+    $rootScope.msg = "Hello2";
   });
-  landing.controller('CraftsController',function($scope){
-    $scope.msg = "Hello3";
+  landing.controller('CraftsController',function($scope, $rootScope){
+    $rootScope.msg = "Hello3";
   });
-  landing.controller('AboutController',function($scope){
-    $scope.msg = "Hello4";
+  landing.controller('AboutController',function($scope, $rootScope){
+    $rootScope.msg = "Hello4";
   });
-  landing.controller('ContactController',function($scope){
-    $scope.msg = "Hello5";
+  landing.controller('ContactController',function($scope, $rootScope){
+    $rootScope.msg = "Hello5";
   });
 }(angular));
